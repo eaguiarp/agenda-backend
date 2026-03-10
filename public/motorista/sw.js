7f40dcba9fe946a0aab6b58c6ca1c9ff3717323a
Eduardo Aguiar <eaguiarp@gmail.com>
2026-03-10 17:25:03 -0300

    motorista

---
+++ public/motorista/sw.js
@@ -0,0 +1,17 @@
+// Service Worker simplificado para o terminal do motorista
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+// Escuta eventos de notificação (para expansões futuras)
+self.addEventListener('push', (event) => {
+  const data = event.data.json();
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: 'icon-192.png'
+  });
+});
