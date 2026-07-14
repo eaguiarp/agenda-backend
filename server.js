bd4f0b0a8aff77efeea6d9ab9ccb81844cd39d01
Eduardo Aguiar <eaguiarp@gmail.com>
2026-07-14 10:47:59 -0300

    ajustes

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 // IMPORTAR as rotas dos vagões
 const vagoesRotas = require('./vagoes_rotas.js');
@@ -107,6 +106,8 @@ app.use((req, res, next) => {
 
   next();
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/eu", auth(1, 'CD Itaborai'), (req, res) => {
   res.json({ usuario: req.auth.user });
